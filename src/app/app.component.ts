7c076770567ed4829941bf71713a887fb7141876
Blinkfrosty <drahman032@gmail.com>
2024-10-26 19:33:40 -0400

    Removing default template code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [LoginComponent],
+  template: '<app-login></app-login>',
 })
 export class AppComponent {
   title = 'MedFinderAdmin';
